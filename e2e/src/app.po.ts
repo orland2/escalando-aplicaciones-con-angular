7d991bdfb5886a66e743a57c1cf1e2c09d1a798f
Orlando Jimenez <orland2@gmail.com>
2018-12-12 12:44:58 -0300

    Test Element Card Titles

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,13 +2,16 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   selectors = {
-    'title' : 'app-root h1'
+    'title': 'app-root h1',
+    'card-titles': 'mat-card-title'
   };
 
   navigateTo() {
     return browser.get('/');
   }
-
+  getCardTitles() {
+    return element.all(by.css(this.selectors['card-titles']));
+  }
   getTitleText() {
     return element(by.css(this.selectors['title'])).getText();
   }
